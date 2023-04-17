60f4b3b8703713c5a3bde481fc7c8b3cc7e80119
hussainahmad1995 <hussainahmadafg@gmail.com>
2023-04-17 18:50:08 -0400

    Also leave the remove header

--- p4gen/genpcap.py
+++ p4gen/genpcap.py
@@ -5,25 +5,6 @@ from scapy.all import Ether, IP, UDP, Padding
 from scapy.all import wrpcap, bind_layers
 
 class PTP(Packet):
-    """Precision Time Protocol"""
-    name = "PTP protocol"
-    fields_desc = [
-        XBitField('transportSpecific', 0x1, 4),
-        XBitField('messageType', 0x0, 4),
-        XBitField('reserved', 0x2, 4),
-        XBitField('versionPTP', 0x2, 4),
-        ShortField('messageLength', 0x2C),
-        XBitField('domainNumber', 0x0, 8),
-        XBitField('reserved2', 0x1, 8),
-        ShortField('flags', 0x0),
-        XBitField('correction', 0x0, 64),
-        IntField('reserved3', 0x0),
-        XBitField('sourcePortIdentity', 0x008063FFFF0009BA, 80),
-        ShortField('sequenceId', 0x9E48),
-        XBitField('PTPcontrol', 0x05, 8),
-        XBitField('logMessagePeriod', 0x0F, 8),
-        XBitField('originTimestamp', 0x000045B111510472F9C1, 80)
-    ]
 
 # bind_layers(UDP, PTP, dport=319)
 bind_layers(Ether, PTP, type=0x88F7)

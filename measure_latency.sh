50b8ded842da0012f362799482c8163472c57596
hussainahmad1995 <hussainahmadafg@gmail.com>
2023-04-20 17:04:44 -0400

    Update Tofino path for measuring latency script

--- measure_latency.sh
+++ measure_latency.sh
@@ -1,48 +1,56 @@
 #!/bin/bash
 
-BMV2_PATH=../../behavioral-model
-P4C_BM_PATH=../../p4c
-PKTGEN_PATH=../pktgen/build/p4benchmark
-P4C_BM_SCRIPT=p4c-bm2-ss
-SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
-CLI_PATH=$BMV2_PATH/tools/runtime_CLI.py
+# BMV2_PATH=../../behavioral-model
+# P4C_BM_PATH=../../p4c
+# PKTGEN_PATH=../pktgen/build/p4benchmark
+# P4C_BM_SCRIPT=p4c-bm2-ss
+# SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
+# CLI_PATH=$BMV2_PATH/tools/runtime_CLI.py
 
+TOFINO_PATH=home/tna-model/bf-sde-9-9-0
+TOFINO_COMPILER_PATH=$SDE
+TOFINO_SCRIPT=$HOME/tools/p4_build.sh
+TOFINO_CLI_PATH=$SDE/run_tofino_model.sh
+TOFINO_SWITCH_PATH=$SDE/run_switchhd.sh
+PKTGEN_PATH=../pktgen/build/p4benchmark
+VETH_SCRIPT=$SDE/install/bin/veth_setup.sh
 
 PROG="main"
 
-read -p "Enter the language version {14|16} = " VERSION
-read -p "No. of Packets to send = " PACKETS
-read -p "Rate of sending packets(bytes/sec) = " RATE
+PACKETS=1000 
+RATE=1000
+# read -p "No. of Packets to send = " PACKETS
+# read -p "Rate of sending packets(bytes/sec) = " RATE
 
 ps -ef | grep simple_switch | grep -v grep | awk '{print $2}' | xargs kill
    
 rm -rf output/
 
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
-do
-	p4benchmark --feature pipeline --tables $i --table-size 32 --version $VERSION
+# for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+# do
+
+	p4benchmark --feature add-header --headers 1
 	
 	cd output
-	
 	set -m
-	$P4C_BM_SCRIPT --std p4-$VERSION $PROG.p4 -o $PROG.json
+	#compile the P4 program
+	# $TOFINO_SCRIPT $PROG.p4
 
 	if [ $? -ne 0 ]; then
-	echo "p4 compilation failed"
+	echo "Tofino compilation for $PROG.p4 failed"
 	exit 1
 	fi
 
 	sudo echo "sudo" > /dev/null
-	sudo $SWITCH_PATH >/dev/null 2>&1
-	sudo $SWITCH_PATH $PROG.json \
-	    -i 0@veth0 -i 1@veth2 -i 2@veth4 -i 3@veth6 -i 4@veth8 \
-	    --log-console &
+	sudo $TOFINO_PATH >/dev/null 2>&1
+	sudo $VETH_SCRIPT
 
 	sleep 2
 	echo "**************************************"
 	echo "Sending commands to switch through CLI"
 	echo "**************************************"
-	$CLI_PATH --json $PROG.json < commands.txt 
+	$TOFINO_CLI_PATH -p $PROG < commands.txt 
+
 
 	echo "READY!!!" 
 	

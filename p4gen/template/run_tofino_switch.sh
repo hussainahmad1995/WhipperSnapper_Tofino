0f21250451862dab80f64f483762809c82551ba8
hussainahmad1995 <hussainahmadafg@gmail.com>
2023-04-24 19:46:51 +0000

    Update the bash script with Tofino paths

--- p4gen/template/run_tofino_switch.sh
+++ p4gen/template/run_tofino_switch.sh
@@ -1,35 +1,35 @@
+#!/bin/bash
 
-BMV2_PATH=../../behavioral-model
-P4C_BM_PATH=../../p4c
-P4C_BM_SCRIPT=p4c-bm2-ss
-
+#Before running the script make sure the $SDE is set 
+TOFINO_PATH=$SDE
+TOFINO_SCRIPT=$SDE/tools/p4_build.sh
+TOFINO_RUN_CLI_PATH=$SDE/tools/run_bfshell.sh
+TOFINO_RUN_SWITCH=$SDE/run_switchhd.sh
+PKTGEN_PATH=../pktgen/build/p4benchmark
+VETH_SCRIPT=$SDE/tools/veth_setup.sh
 
 PROG="main"
 
-read -p "Enter the language version {p4-14|p4-16} = " VERSION
-
 set -m
-$P4C_BM_SCRIPT --std $VERSION $PROG.p4 -o $PROG.json
+#compile the P4 program
+# $TOFINO_SCRIPT $PROG.p4
 
 if [ $? -ne 0 ]; then
 echo "p4 compilation failed"
 exit 1
 fi
 
-SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
-
-CLI_PATH=$BMV2_PATH/tools/runtime_CLI.py
-
 sudo echo "sudo" > /dev/null
-sudo $SWITCH_PATH >/dev/null 2>&1
-sudo $SWITCH_PATH $PROG.json \
-    -i 0@veth0 -i 1@veth2 -i 2@veth4 -i 3@veth6 -i 4@veth8 \
-    --log-console &
+#set up virtual ethernet ports
+sudo .$VETH_SCRIPT 
+
+#
+sudo $TOFINO_SWITCH_RUN -p $PROG >/dev/null 2>&1
 
 sleep 2
 echo "**************************************"
 echo "Sending commands to switch through CLI"
 echo "**************************************"
-$CLI_PATH --json $PROG.json < commands.txt
+./$TOFINO_CLI_PATH $PROG < commands.txt
 echo "READY!!!"
 fg
